aa6529476d03c8888ae9d0b4c167fb6350551a2a
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-02-20 02:38:20 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ((array === undefined)
+    || (array.length === 0)) {
+        return 0;
+    }
+
+    const arr = [...array];
+    return arr.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ((array === undefined)
+    || (array.length === 0)) {
+        return 0;
+    }
+
+    const arr = [...array];
+    return arr.sort((a, b) => b - a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ((array === undefined)
+    || (array.length === 0)) {
+        return 0;
+    }
+
+    const sum = array.reduce((acc, elem) => acc += elem, 0);
+    const average = sum / array.length;
+    return average;
 }
